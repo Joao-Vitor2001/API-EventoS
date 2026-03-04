ccdb977489f8e77cc6279d48321b6a804e71ea42
Joao Vitor <joaovitorfreiredasilva@gmail.com>
2026-03-04 20:42:22 -0300

    fix awards service

--- api/SERVICES/awardsService.js
+++ api/SERVICES/awardsService.js
@@ -3,19 +3,31 @@ const path = require('path');
 
 const filePath = path.join(__dirname, '../data/awards.json');
 
-function getAwards() {
-    const data = fs.readFileSync(filePath);
-    return JSON.parse(data);
+function lerArquivo() {
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, "[]");
+    }
+
+    const data = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(data || "[]");
 }
 
-function saveAward(award) {
-    const awards = getAwards();
-    awards.push(award);
+function salvarArquivo(dados) {
+    fs.writeFileSync(filePath, JSON.stringify(dados, null, 2));
+}
 
-    fs.writeFileSync(filePath, JSON.stringify(awards, null, 2));
+function listar() {
+    return lerArquivo();
+}
+
+function criar(novoAward) {
+    const awards = lerArquivo();
+    awards.push(novoAward);
+    salvarArquivo(awards);
+    return novoAward;
 }
 
 module.exports = {
-    getAwards,
-    saveAward
+    listar,
+    criar
 };
